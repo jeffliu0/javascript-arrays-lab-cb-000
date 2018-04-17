8eebea1bb04bdbad38abbcaa48ae1b222d015324
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-17 06:07:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,23 @@ function destructivelyRemoveFirstKitten()
   kittens.shift()
   return kittens
 }
+
+function appendKitten(name)
+{
+  return [...kittens, name]
+}
+
+function prependKitten(name)
+{
+  return [name, ...kittens]
+}
+
+function removeLastKitten()
+{
+  return kittens.slice(kittens.length - 1)
+}
+
+function removeFirstKitten()
+{
+  return kittens.slice(1)
+}
